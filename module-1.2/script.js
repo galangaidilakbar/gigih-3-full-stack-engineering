aa882527fed4092099aaf21634657268f48cd3f3
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-07-05 20:56:40 +0700

    Update script.js

--- module-1.2/script.js
+++ module-1.2/script.js
@@ -1,43 +1,52 @@
 import axios from 'axios';
 
-// Simulate handling promise success
-// axios.get('https://jsonplaceholder.typicode.com/todos')
-//     .then(response => {
-//         response.data.forEach(({title}, index) => {
-//             index++;
-//             console.log(index, title)
-//         });
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     });
+// Simulating promise success
+axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then(response => {
+        response.data.forEach(({title}, index) => {
+            index++;
+            console.log(index, title)
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
 
-// Simulate handling promise error
+// This code will be executed first, even though it is written after the promise.
+console.log('I am the first one to be executed, Not you Promise! 😜');
+
+// Simulating promise error
 // axios.get('https://httpstat.us/404')
 //     .then(response => {
-//         //
+//         // this block will not be executed
+//         console.log(response.data)
 //     })
 //     .catch(error => {
 //         console.log(error.message);
 //     });
 
-// Simulate handling async/await success
+// Simulating async/await success
 // async function getTodos() {
 //     try {
 //         const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+//
 //         response.data.forEach(({title}, index) => {
 //             index++;
 //             console.log(index, title)
 //         });
+//
+//         // this code will be executed after the promise resolved
+//         console.log('I am the last one to be executed')
 //     } catch (error) {
 //         console.log(error);
 //     }
 // }
 
-// Simulate handling async/await error
+// Simulating async/await error
 // async function getTodos() {
 //     try {
 //         const response = await axios.get('https://httpstat.us/500');
+//         // this block will not be executed
 //         response.data.forEach(({title}, index) => {
 //             index++;
 //             console.log(index, title)
@@ -46,5 +55,5 @@ import axios from 'axios';
 //         console.log(error.message);
 //     }
 // }
-
-getTodos();
+//
+// getTodos();
