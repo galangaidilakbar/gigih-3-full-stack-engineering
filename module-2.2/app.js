672e594a86bd42098204a6d5ed6f351d1b3aac67
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-07-13 02:32:45 +0700

    Add songs router

--- module-2.2/app.js
+++ module-2.2/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 const playlistsRouter = require('./routes/apis/playlists');
+const songsRouter = require('./routes/apis/songs');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/v1/playlists', playlistsRouter);
+app.use('/api/v1/songs', songsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ module-2.2/routes/apis/songs.js
@@ -0,0 +1,22 @@
+const songsData = require('../../datasets/songsData.json');
+const express = require('express');
+const router = express.Router();
+
+
+/* GET song by id */
+router.get('/:id', function (req, res) {
+    const song = songsData.find((song) => song.id === parseInt(req.params.id));
+
+    if (!song) {
+        return res.status(404).json({
+            message: "Song not found",
+        });
+    }
+
+    return res.json({
+        message: "Song retrieved successfully",
+        data: song
+    });
+});
+
+module.exports = router;
