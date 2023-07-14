ac4db031ccad9079bbd046e38ae5e389d48c62e0
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-07-14 16:11:53 +0700

    Get list of songs form playlist
    play song from the playlists

--- module-2.2/routes/index.js
+++ module-2.2/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Song App' });
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Song App'});
+});
+
+/* GET playlist page. */
+router.get('/playlists/:id', function (req, res) {
+    res.render('playlist', {
+        title: 'Playlist - Song App',
+        playlistId: req.params.id
+    });
 });
 
 module.exports = router;
