9ab7023476da85663a69bff804eec89c4a5b064d
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-07-21 18:52:39 +0700

    set up mongo connection

--- module-3.3/index.js
+++ module-3.3/index.js
@@ -1,4 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
+
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString)
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', () => console.log('Connected to MongoDB'));
 
 const app = express();
 
